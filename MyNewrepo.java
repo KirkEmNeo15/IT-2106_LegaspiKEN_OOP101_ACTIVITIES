59835dd89f4c251333b228ff81ce949196d3a629
KirkEmNeo15 <saichu1115@gmail.com>
2026-09-23 09:45:57 +0800

    Update MyNewrepo.java

--- MyNewrepo.java
+++ MyNewrepo.java
@@ -1,3 +1,11 @@
-public class MyNewrepo {
-    
+import java.util.Scanner;
+public class MyNewRepo {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.printIn("Hello, World");
+            System.out.printIn("Enter Your Name:");
+            String name = input.nextline();
+            System.out.printIn("Nice to meet you, " + name + "!");
+        }
+    }
 }
